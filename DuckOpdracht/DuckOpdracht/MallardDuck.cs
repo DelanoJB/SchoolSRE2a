4fdf00263ecbf88e3b6d8a7fdc06d09ea06b6021
DelanoJB <delanojbrasz@gmail.com>
2018-04-09 14:12:01 +0200

    Add files via upload

---
+++ DuckOpdracht/DuckOpdracht/MallardDuck.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public class MallardDuck: Duck
+    {
+           public MallardDuck()
+    {
+        this.quackBehavior = new Quack();
+        this.flyBehavior = FlyWithWings(); 
+    }
+        public void display()
+    {
+        Console.WriteLine("I'm a real Mallard Duck");
+    }
+    }
+}
